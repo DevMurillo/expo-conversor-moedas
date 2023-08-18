cf854dd07a935abdfa5b4d1dddcbe4ac2b4534b7
DevMurillo <141368407+DevMurillo@users.noreply.github.com>
2023-08-18 11:20:26 +0000

    app mudado

--- App.js
+++ App.js
@@ -8,18 +8,41 @@ export default function App() {
   const [moedaDestino, setMoedaDestino] = useState('USD')
   const [valorEntrada, setVAlorEntrada] = useState('33.33')
   const [resultado, setResultado] = useState('')
+
+  const handleConverter = async () => {
+    let URL = `https://economia.awesomeapi.com.br/last/${moedaOrigem}-${moedaDestino}`;
+    try {
+      let page = await fetch(URL);
+      let json = await page.json();
+      //console.log(json);
+      let indice = parseFloat(json[`${moedaOrigem}${moedaDestino}`].high);
+      //setResultado(indice)
+      let valor = parseFloat(valorEntrada);
+      setResultado((valor *indice).toFixed(2))
+    } catch (error) {
+      setResultado(`Erro: ${error.message}`)
+    }
+   }
+
+  const handleLimpar = () => {
+    setResultado('');
+    setVAlorEntrada('33.33333');
+    setMoedaOrigem('BRL');
+    setMoedaDestino('USD');
+   }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Conversor de Moedas</Text>
+      <Text style={styles.title}>Convesor de Moedas</Text>
       <View>
         <Text style={styles.tbMoeda}>Moeda 1</Text>
         <Picker
-        style={styles.picker}
+          style={styles.picker}
           selectedValue={moedaOrigem}
           onValueChange={(itemValue, itemIndex) =>
             setMoedaOrigem(itemValue)
           }>
-          <Picker.Item label="Real Brasileiro" value="BRL" />
+            <Picker.Item label="Real Brasileiro" value="BRL" />
           <Picker.Item label="Dólar Americano" value="USD" />
           <Picker.Item label="Bitcoin" value="BTC" />
           <Picker.Item label="Dogecoin" value="DOGE" />
@@ -30,12 +53,12 @@ export default function App() {
       <View>
         <Text style={styles.tbMoeda}>Moeda 2</Text>
         <Picker
-        style={styles.picker}
+          style={styles.picker}
           selectedValue={moedaDestino}
           onValueChange={(itemValue, itemIndex) =>
             setMoedaDestino(itemValue)
           }>
-          <Picker.Item label="Real Brasileiro" value="BRL" />
+             <Picker.Item label="Real Brasileiro" value="BRL" />
           <Picker.Item label="Dólar Americano" value="USD" />
           <Picker.Item label="Bitcoin" value="BTC" />
           <Picker.Item label="Dogecoin" value="DOGE" />
@@ -44,20 +67,21 @@ export default function App() {
         </Picker>
       </View>
       <View>
-        <Text style={styles.tbMoeda}>Valor para Conversão</Text>
-        <TextInput 
-        style={styles.input} 
-        value={valorEntrada} 
-        onChangeText={setVAlorEntrada} 
-        keyboardType='numeric'>
+        <Text style={styles.tbMoeda}>Valo para Conversão</Text>
+        <TextInput
+          style={styles.input}
+          value={valorEntrada}
+          onChangeText={setVAlorEntrada}
+          keyboardType='numeric'>
         </TextInput>
       </View>
-      <Pressable style={styles.button}>
-        <Text style={styles.title}>Converter</Text>
-        </Pressable>
-        <Pressable style={styles.button}> 
+      <Pressable onPress={handleConverter} style={styles.button}>
+        <Text style={styles.title}>Conveter</Text>
+      </Pressable>
+      <Pressable onPress={handleLimpar} style={styles.button}>
         <Text style={styles.title}>Limpar</Text>
-        </Pressable>
+      </Pressable>
+      <View><Text style={styles.lbResultado}>{resultado}</Text></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -71,30 +95,34 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    color: '#fff',
+    color: '#fff'
   },
   picker: {
     color: '#fff',
     width: 200,
-    height:50,
-    backgroundColor: '#000',
+    height: 50,
+    backgroundColor: '#000'
   },
   input: {
     color: '#fff',
     textAlign: 'right',
     height: 40,
+    width: 200
   },
   tbMoeda: {
-    color: '#fff',
+    color: '#fff'
   },
   button: {
     width: 200,
     height: 40,
     paddingBottom: 10,
-    backgroundColor:'#999',
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius: 5,
+    backgroundColor: '#999',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5
   },
+  lbResultado: {
+    color: '#fff'
+  }
 });
